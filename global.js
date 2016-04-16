d8865f955223717054128c21e7d7757c04b46fac
onurcaglayan <onur.caglayan@msn.com>
2016-04-16 00:34:49 -0300

    Test Docs

---
+++ global.js
@@ -0,0 +1,11 @@
+/* jshint esnext: true */
+
+var path = require("path");
+
+console.log(`Dosya isimi: ${path.basename(__filename)}`);
+
+
+function innerFunction() {
+    
+    return `several things to display`;
+}
